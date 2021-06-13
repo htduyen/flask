fbb49ead87a52733f2b645b73ed9bd70a49e102e
htduyen <thanhduyeny1@gmail.com>
2021-06-13 20:29:41 +0700

    Chapter2 + add git ignore - using render_template - conditional statement

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-    return "Hello, World!"
+    title = 'Chapter2'
+    persion = {
+        'username': 'Huynh Thanh Duyen',
+        'age': 24
+    }
+    posts = [
+        {
+           'name': 'Duyen Huynh',
+           'content': 'Example 1'
+        },
+        {
+           'name': 'Duyen Huynh 2',
+           'content': 'Example 2'
+        },
+        {
+           'name': 'Duyen Huynh 3',
+           'content': 'Example 3'
+        }
+    ]
+    return render_template('index.html', username=persion['username'], age=persion['age'], posts=posts)
