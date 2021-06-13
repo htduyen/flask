1a4a73468ff87f1380bc891a9508a249dc00dfa0
htduyen <thanhduyeny1@gmail.com>
2021-06-13 22:02:00 +0700

    Using web form, redirect, url_for

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, url_for
+from werkzeug.utils import redirect
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -26,3 +28,13 @@ def index():
         }
     ]
     return render_template('index.html', username=persion['username'], age=persion['age'], posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
